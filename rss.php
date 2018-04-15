933e1d0dea2b5a43011f1d092e3a54484f418635
Baptiste Candellier <outadoc@gmail.com>
2018-04-15 18:59:43 +0100

    Add license, fix encoding

--- rss.php
+++ rss.php
@@ -1,4 +1,22 @@
 <?php
+/*
+    Borguerie Center
+    Copyright (C) 2011–2013  Baptiste Candellier
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 header("Content-Type:text/xml");
 echo '<rss version="2.0" encoding="iso-8859-1">';
 echo("<channel>
